39d26aeddafaee2baba870e7274279903090cbb3
eugeneleejunping <eugeneleejunping@gmail.com>
2025-04-22 12:00:10 +0800

    add while loop solution

--- array/insert_interval.py
+++ array/insert_interval.py
@@ -33,3 +33,27 @@ class Solution:
             res.append(intervals[k])
 
         return res
+
+        """
+        if newInterval[0]<intervals[i][1], transform, newInterval=newInterval
+        time complexity: O(N)
+        space complexity: O(N)
+        """
+        res: list[list[int]] = []
+        # case 1: intervals[i][1]<newInterval[0]
+        i: int = 0
+        n: int = len(intervals)
+        while i < n and intervals[i][1] < newInterval[0]:
+            res.append(intervals[i])
+            i += 1
+        # case 2: when newInterval[1]>=intervals[i][0];
+        # no need to check for 2nd index val of newInterval since it will be <= intervals[i][1] based on check in case 1
+        while i < n and newInterval[1] >= intervals[i][0]:
+            newInterval[0] = min(newInterval[0], intervals[i][0])
+            newInterval[1] = max(newInterval[1], intervals[i][1])
+            i += 1
+        res.append(newInterval)
+        while i < n and newInterval[1] < intervals[i][0]:
+            res.append(intervals[1])
+            i += 1
+        return res
