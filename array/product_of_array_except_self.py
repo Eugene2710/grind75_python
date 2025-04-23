f02c747a1f08fa6c4c747b031e10163b2677aeba
eugeneleejunping <eugeneleejunping@gmail.com>
2025-04-23 13:43:17 +0800

    redid questions and made v minor edits

--- array/product_of_array_except_self.py
+++ array/product_of_array_except_self.py
@@ -18,3 +18,17 @@ class Solution:
 
         return res
 
+        # length: int = len(nums)
+        # res: list[int] = [1]*length
+        #
+        # # get list with product of all elems in nums list to left of indexed elem
+        # for i in range(1, length):
+        #     res[i] = res[i-1]*nums[i-1]
+        # # nums: list[int] = [1,2,3,4]   expected ans = [24, 12, 8, 6]
+        # # res: list[int] = [1,1,2,6]
+        #
+        # left_product: int = 1
+        # for i in range(length-1, -1, -1):
+        #     res[i] *= left_product
+        #     left_product *= nums[i]
+        # return res
--- array/three_sum.py
+++ array/three_sum.py
@@ -5,6 +5,7 @@ class Solution:
         sort the input array
         using nested loops, and three pointers, for each outer loop, use the two sum II appraoch to check if sum is >/</= target
         keep moving first pointer to right, and repeat
+        time complexity: O(NlogN) + O(N^2) =
 
         nums = [-1,0,1,2,-1,-4]
         after sorting -> [-4,-1,-1,0,1,2]
@@ -34,3 +35,28 @@ class Solution:
                     right-=1
         return res
 
+        nums.sort()
+        res: list[list[int]] = []
+        total: int = 0
+        # iterate to the third last element since there are 2 more pointers that will reach the last and second last
+        for i in range(len(num)-2):
+            # to skip duplicate elements earlier on
+            if i>0 and nums[i] == nums[i-1]:
+                continue
+            left: int = i+1
+            right: int = len(nums) - 1
+            while left < right:
+                total = nums[i] + nums[left] + nums[right]
+                if total == 0:
+                    res.append([nums[i], nums[left], nums[right]])
+                    while left < right and nums[left] == nums[left+1]:
+                        left += 1
+                    while left <  right and nums[right] == nums[right-1]:
+                        right -= 1
+                    left += 1
+                    right -= 1
+                elif total < 0:
+                    left += 1
+                else:
+                    right -= 1
+        return res
